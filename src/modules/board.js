07c0354d4a384c862950dd018e54beb3f399dfd6
Will H <will.wkhu@gmail.com>
2023-02-16 22:48:57 -0800

    Add Gameboard class

--- src/modules/board.js
+++ src/modules/board.js
@@ -1,18 +1,41 @@
-import Ship from './ship';
-
 class Gameboard {
   constructor() {
     this.ships = [];
-    this.attacks = [];
-    this.madeAttacks = [];
-    this.missedAttacks = [];
+    this.hits = [];
+
+    // Add this for AI smart target coord?
+    // Remove sunk property from ship and add shiplsLost?
+    this.shipsLost = 0;
+    // this.danger = false;
   }
 
-  receiveAttack() {}
+  placeShip(ship) {
+    this.ships.push(ship);
+  }
 
-  placeShip() {
-    const shipType = new Ship();
-    this.ships.push(shipType);
+  receiveAttack(oppCoord) {
+    this.ships.forEach((ship) => {
+      ship.position.forEach((coord) => {
+        if (coord === oppCoord) {
+          this.hits.push(oppCoord);
+          ship.hit();
+          if (ship.isSunk()) {
+            this.shipsLost += 1;
+            if (this.isAllSunk()) {
+              console.log('No ships left');
+            }
+          }
+        }
+      });
+    });
+  }
+
+  // isAllSunk() {
+  //   return this.ships.every((ship) => ship.sunk === true);
+  // }
+
+  isAllSunk() {
+    return this.shipsLost.length === this.ships.length;
   }
 }
 
