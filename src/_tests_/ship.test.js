cbe88da966e8641edd4bd5dde1c9eb040a824171
Will H <will.wkhu@gmail.com>
2023-03-01 21:41:26 -0800

    Update ship tests

--- src/_tests_/ship.test.js
+++ src/_tests_/ship.test.js
@@ -1,28 +1,34 @@
 import Ship from '../modules/ship';
 
 describe('Ship object', () => {
-  const destroyer = new Ship('Destroyer', [1, 2, 3]);
+  const destroyer = new Ship('Destroyer', 3);
 
-  test('properties', () => {
+  test('Properties', () => {
     expect(destroyer).toEqual({
       name: 'Destroyer',
-      position: [1, 2, 3],
-      damage: 0,
+      length: 3,
+      position: [],
+      damage: [],
     });
   });
 
-  test('hit method', () => {
-    destroyer.hit();
-    destroyer.hit();
-    expect(destroyer).toHaveProperty('damage', 2);
+  test('A hit', () => {
+    destroyer.position = [1, 2, 3];
+    destroyer.hit(3);
+    expect(destroyer).toHaveProperty('damage', [3]);
   });
 
-  test('isSunk shows false', () => {
+  test('Another hit', () => {
+    destroyer.hit(1);
+    expect(destroyer).toHaveProperty('damage', [1, 3]);
+  });
+
+  test('Ship not sunk', () => {
     expect(destroyer.isSunk()).toBe(false);
   });
 
-  test('isSunk shows true', () => {
-    destroyer.hit();
+  test('Ship sinks', () => {
+    destroyer.hit(2);
     expect(destroyer.isSunk()).toBe(true);
   });
 });
