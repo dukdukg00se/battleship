f5a86126bd9cc958378bbf01ee578d4e15e4e047
Will H <will.wkhu@gmail.com>
2023-03-02 14:17:18 -0800

    Update tests

--- src/_tests_/player.test.js
+++ src/_tests_/player.test.js
@@ -13,8 +13,7 @@ describe('Player object', () => {
         name: 'player1',
         board: {
           fleet: [],
-          hits: [],
-          shipsLost: 0,
+          shipsLost: [],
         },
         attacks: [],
       });
@@ -25,13 +24,29 @@ describe('Player object', () => {
     const player2 = new Player();
 
     test('Opponent receives attack', () => {
-      const called = jest.spyOn(player2.board, 'receiveAttack');
+      const receiveAttack = jest.spyOn(player2.board, 'receiveAttack');
       player1.attack(player2, 2);
-      expect(called).toHaveBeenCalled();
+      expect(receiveAttack).toHaveBeenCalled();
     });
 
     test('Attack is logged', () => {
       expect(player1.attacks).toEqual([2]);
+    });
+  });
+
+  describe('Assembling fleet', () => {
+    test('Correct number of ships', () => {
+      player1.assembleFleet();
+      expect(player1.board.fleet).toHaveLength(5);
+    });
+
+    test('Fleet has a carrier', () => {
+      expect(player1.board.fleet).toContainEqual({
+        name: 'carrier',
+        length: 5,
+        position: [],
+        damage: [],
+      });
     });
   });
 });
