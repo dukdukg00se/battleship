7fce4ea07b70610a68086e6c984857cb640c328d
Will H <will.wkhu@gmail.com>
2023-03-02 14:15:14 -0800

    Update tests

--- src/_tests_/board.test.js
+++ src/_tests_/board.test.js
@@ -4,79 +4,63 @@ import Ship from '../modules/ship';
 describe('Gameboard object', () => {
   let board;
   let ship1;
-  let ship2;
 
   beforeEach(() => {
     board = new Gameboard();
-    ship1 = new Ship('a', [1, 2]);
-    ship2 = new Ship('b', [3, 4]);
+    ship1 = new Ship('a', 3);
+    ship1.position = [3, 4, 5];
   });
 
   describe('Board', () => {
     test('Properties', () => {
       expect(board).toEqual({
         fleet: [],
-        hits: [],
-        shipsLost: 0,
-      });
-    });
-  });
-
-  describe('Adding ships', () => {
-    test('Fleet property', () => {
-      board.addShip(ship1);
-      board.addShip(ship2);
-
-      expect(board).toEqual({
-        fleet: [
-          {
-            name: 'a',
-            position: [1, 2],
-            damage: 0,
-          },
-          {
-            name: 'b',
-            position: [3, 4],
-            damage: 0,
-          },
-        ],
-        hits: [],
-        shipsLost: 0,
+        shipsLost: [],
       });
     });
   });
 
   describe('What happens when attacked', () => {
     beforeEach(() => {
-      board.addShip(ship1);
+      board.fleet.push(ship1);
     });
 
     test('Correctly log hits', () => {
-      board.receiveAttack(21);
-      board.receiveAttack(2);
-      expect(board).toHaveProperty('hits', [2]);
+      board.receiveAttack(5);
+      expect(board).toHaveProperty('fleet', [
+        { name: 'a', length: 3, position: [3, 4, 5], damage: [5] },
+      ]);
     });
 
     test('Check if ship sunk after a hit', () => {
       const isSunkCalled = jest.spyOn(ship1, 'isSunk');
-      board.receiveAttack(2);
+      board.receiveAttack(79);
+      expect(isSunkCalled).toHaveBeenCalledTimes(0);
+    });
+
+    test("Don't check if ship sunk after a miss", () => {
+      const isSunkCalled = jest.spyOn(ship1, 'isSunk');
+      board.receiveAttack(5);
       expect(isSunkCalled).toHaveBeenCalled();
     });
 
     test('Checks fleet only after ship is sunk', () => {
-      const allSunk = jest.spyOn(board, 'isAllSunk');
-      board.receiveAttack(1);
-      board.receiveAttack(2);
+      const allSunk = jest.spyOn(board, 'fleetLost');
+      board.receiveAttack(3);
+      board.receiveAttack(4);
+      board.receiveAttack(5);
+      board.receiveAttack(6);
       expect(allSunk).toHaveBeenCalledTimes(1);
     });
 
     test('Correctly tell when fleet eliminated', () => {
-      board.receiveAttack(1);
-      let allSunk = board.isAllSunk();
+      board.receiveAttack(3);
+      board.receiveAttack(4);
+      let allSunk = board.fleetLost();
       expect(allSunk).toBe(false);
 
-      board.receiveAttack(2);
-      allSunk = board.isAllSunk();
+      board.receiveAttack(5);
+      allSunk = board.fleetLost();
       expect(allSunk).toBe(true);
     });
   });
