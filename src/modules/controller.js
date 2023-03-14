2bbc9cabab610e682a7ce14dce90a076c1bdf2ed
Will H <will.wkhu@gmail.com>
2023-03-13 22:58:49 -0700

    Refactor fns, add btns listeners
    - Renamed typewriter fn to something indicative of its use.
    - Refactored fn to place player ships.
    - Add listeners to player btns to run corresponding feature.

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,11 +1,12 @@
 import Player from './player';
 import AIPlayer from './ai-player';
-import { getCeiling, isRedundant } from './helpers';
+import { getCeiling, isCoordsEligible } from './helpers';
+import createBattlePage from './display/battle';
 
 /* eslint-disable */
 
 function changeAxis() {
-  const axisBtn = document.querySelector('.info-wrap + button');
+  const axisBtn = document.querySelector('#axis');
   axisBtn.onclick = (e) => {
     if (e.target.dataset.axis === 'x') {
       e.target.dataset.axis = 'y';
@@ -17,14 +18,14 @@ function changeAxis() {
   };
 }
 
-function typeWriter(text, i = 0) {
+function msgPlayer(text, i = 0) {
   if (i > text.length) return;
-  const typeWriterElement = document.querySelector('.typewriter');
-  typeWriterElement.textContent = text.substring(0, (i += 1));
+  const headMsg = document.querySelector('.prompt');
+  headMsg.textContent = text.substring(0, (i += 1));
 
   setTimeout(() => {
-    typeWriter(text, (i += 1));
-  }, 80);
+    msgPlayer(text, i);
+  }, 30);
 }
 
 function lockSqs() {
@@ -36,44 +37,41 @@ function lockSqs() {
   });
 }
 
-function isSqEligible(selectArr, excludeArr, max) {
-  if (selectArr.some((element) => excludeArr.includes(element))) return false;
-  if (selectArr[selectArr.length - 1] > max) return false;
-  return true;
+function startBattle() {
+  
+
+  let positGrid = document.querySelector('.grid');
+  positGrid.style.animation = '1s ease-in 0s 1 normal none running fadeout';
+
+  setTimeout(() => {
+    positGrid.remove();
+    const main = document.querySelector('main');
+    main.append(createBattlePage());
+    initNewGame(name);
+  }, 980)
 }
 
-export default function initNewGame(name) {
-  const player1 = new Player(name);
-  const computer = new AIPlayer();
-
-  player1.assembleFleet();
-  computer.assembleFleet();
-  computer.positionFleet();
-
-  const currentShipIndex = 0;
-  const excludeCoords = [];
-
-  changeAxis();
-  placeShip(currentShipIndex, player1, excludeCoords);
-}
-
-function placeShip(index, player, exclude) {
-  const name = player.name;
+function placeShips(index, player, exclude) {
+  const { name } = player;
   const ships = player.board.fleet;
   const grid = document.querySelector('.grid');
   let shipLocation = [];
-  let ship = ships[index];
+  const ship = ships[index];
+  let msg;
 
-  console.log(ship);
 
   if (index >= ships.length) {
-    // run the game
+    msg = `Admiral ${name}, fleet ready. Press battle to continue.`;
+    msgPlayer(msg.toUpperCase());
+    
     grid.onmouseover = null;
     console.log('start game');
+
+
     return;
   } else {
-    const msg = `Admiral ${name}, position your ${ship.name}.`;
-    typeWriter(msg);
+    msg = `Admiral ${name}, position your ${ship.name}.`;
+    msgPlayer(msg.toUpperCase());
 
     let startCoord;
     let axis;
@@ -85,7 +83,7 @@ function placeShip(index, player, exclude) {
       if (sq) {
         const reqCoords = [];
         startCoord = +sq.dataset.nmbr;
-        axis = document.querySelector('.info-wrap + button').dataset.axis;
+        axis = document.querySelector('#axis').dataset.axis;
         max = getCeiling(startCoord, axis === 'x');
 
         for (let i = 0; i < ship.length; i += 1) {
@@ -95,39 +93,24 @@ function placeShip(index, player, exclude) {
 
         shipLocation = [...reqCoords];
 
-        if (isSqEligible(shipLocation, exclude, max)) {
+        if (isCoordsEligible(shipLocation, exclude, max)) {
           shipLocation.forEach((coord) => {
-            let targSq = document.querySelector(`[data-nmbr="${coord}"]`);
+            const targSq = document.querySelector(`[data-nmbr="${coord}"]`);
             targSq.classList.remove('lock');
             targSq.classList.add('unlock');
           });
-
-          // sq.onclick = () => {
-          //   ship.position = shipLocation;
-          //   index += 1;
-          //   exclude.push(...ship.position)
-
-          //   shipLocation.forEach((coord) => {
-          //     let targSq = document.querySelector(`[data-nmbr="${coord}"]`);
-          //     targSq.classList.remove('unlock');
-          //     targSq.classList.add('lock');
-          //     targSq.classList.add('placed');
-          //   });
-
-          //   placeShip(index, player, exclude)
-          // }
         }
       }
     };
 
     setTimeout(() => {
       grid.onclick = () => {
-        if (isSqEligible(shipLocation, exclude, max)) {
+        if (isCoordsEligible(shipLocation, exclude, max)) {
           ship.position = shipLocation;
           exclude.push(...ship.position);
 
           shipLocation.forEach((coord) => {
-            let targSq = document.querySelector(`[data-nmbr="${coord}"]`);
+            const targSq = document.querySelector(`[data-nmbr="${coord}"]`);
             targSq.classList.remove('unlock');
             targSq.classList.add('lock');
             targSq.classList.add('placed');
@@ -135,13 +118,74 @@ function placeShip(index, player, exclude) {
 
           if (ship.position.length > 0) {
             index += 1;
-            placeShip(index, player, exclude);
+            placeShips(index, player, exclude);
           }
         }
       };
-    }, 1600);
+    }, 1400);
 
     grid.onmouseout = lockSqs;
-    // grid.addEventListener('mouseout', lockSqs);
   }
+}
+
+export default function initNewGame(name) {
+  const player1 = new Player(name);
+  const computer = new AIPlayer();
+
+  player1.assembleFleet();
+  computer.assembleFleet();
+  computer.positionFleet();
+
+  let currentShipIndex = 0;
+  let excludeCoords = [];
+
+  placeShips(currentShipIndex, player1, excludeCoords);
+
+  const btns = document.querySelectorAll('button');
+  btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+
+      if (e.target.id === 'axis') {
+        if (e.target.dataset.axis === 'x') {
+          e.target.dataset.axis = 'y';
+          e.target.textContent = 'AXIS: Y';
+        } else {
+          e.target.dataset.axis = 'x';
+          e.target.textContent = 'AXIS: X';
+        }
+      }
+
+      if (e.target.id === 'clear') {
+        currentShipIndex = 0;
+        excludeCoords = [];
+
+        const sqs = document.querySelectorAll('.sq');
+        sqs.forEach((sq) => {
+
+          if (sq.classList.contains('placed')) {
+            sq.classList.add('cleared');
+          }
+
+          setTimeout(() => {
+            sq.classList.remove('placed');
+            sq.classList.remove('cleared');
+            sq.classList.remove('unlock');
+            sq.classList.add('lock');
+          }, 500)
+
+          // sq.classList.remove('placed');
+          // sq.classList.remove('unlock');
+          // sq.classList.add('lock');
+        
+        });
+
+        placeShips(currentShipIndex, player1, excludeCoords);
+      }
+
+      if (e.target.id === 'random') {
+        
+      }
+
+    })
+  })
 }
