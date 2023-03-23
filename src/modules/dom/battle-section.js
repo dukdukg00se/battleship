77be0dbe1bf37099296236f70c5fa4fcede011d1
Will H <will.wkhu@gmail.com>
2023-03-22 22:58:25 -0700

    Remove createGrid fns, code to show ship placement

--- src/modules/dom/battle-section.js
+++ src/modules/dom/battle-section.js
@@ -1,28 +1,6 @@
-/* eslint-disable */
-function createGrid(maxSqs = 100) {
-  const gridContainer = document.createElement('div');
-  gridContainer.classList.add('grid');
-  for (let i = 1; i <= maxSqs; i += 1) {
-    const sq = document.createElement('div');
-    sq.dataset.nmbr = i;
-    sq.classList.add('sq');
-    gridContainer.append(sq);
-  }
-  return gridContainer;
-}
+import { createGrid, addClassToSqs } from "./positioning-section";
 
-function createOppGrid(maxSqs = 100) {
-  const gridContainer = document.createElement('div');
-  gridContainer.classList.add('grid');
-  for (let i = 1; i <= maxSqs; i += 1) {
-    const sq = document.createElement('div');
-    sq.dataset.nmbr = i;
-    sq.classList.add('sq');
-    sq.classList.add('target');
-    gridContainer.append(sq);
-  }
-  return gridContainer;
-}
+/* eslint-disable */
 
 function markShot() {
   const circle = document.createElement('div');
@@ -30,40 +8,23 @@ function markShot() {
   return circle;
 }
 
-function createBattlePage(player) {
+function createBattlePage(player, opponent) {
   const gridsContainer = document.createElement('div');
   const playerWrapper = document.createElement('div');
   const oppWrapper = document.createElement('div');
   const playerHeader = document.createElement('h3');
   const oppHeader = document.createElement('h3'); 
   const playerGrid = createGrid()
-  const oppGrid = createOppGrid();
+  const oppGrid = createGrid();
 
-  playerWrapper.classList.add('player-wrapper');
-  oppWrapper.classList.add('opp-wrapper')
-  playerHeader.textContent = 'FRIENDLY WATERS';
-  oppHeader.textContent = 'ENEMY WATERS';
   gridsContainer.classList.add('grid-container');
-  playerGrid.classList.add('player');
-  oppGrid.classList.add('opponent');
-
-  // Move this to the DOM module
-  player.board.fleet.forEach(ship => {
-    ship.position.forEach(coord => {
-      const targSq = playerGrid.querySelector(`[data-nmbr="${coord}"]`);
-      targSq.classList.add('placed');
-      targSq.dataset.ship = ship.name;
-    })
-  })
-
-  // const playerShips = player.board.fleet.reduce((coords, ship) => {
-  //   coords.push(...ship.position);
-  //   return coords;
-  // }, []);
-  // playerShips.forEach(coord => {
-  //   const targSq = playerGrid.querySelector(`[data-nmbr="${coord}"]`);
-  //   targSq.classList.add('placed');
-  // })
+  playerWrapper.classList.add('player-wrapper');
+  playerHeader.textContent = 'FRIENDLY WATERS';
+  playerGrid.classList.add(player.name, 'battle-grid');
+  oppWrapper.classList.add('opp-wrapper')
+  oppHeader.textContent = 'ENEMY WATERS';
+  oppGrid.classList.add(opponent.name, 'battle-grid');
+  addClassToSqs(oppGrid, 'target');
 
   playerWrapper.append(playerHeader, playerGrid);
   oppWrapper.append(oppHeader, oppGrid);
