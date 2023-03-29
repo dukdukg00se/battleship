2af86afbe3c08cd5205517c0725a42c3dded3eed
Will H <will.wkhu@gmail.com>
2023-03-28 23:18:05 -0700

    Add a winner section to declare winner
    Styled winner section and removed some comments from styles.

---
+++ src/modules/dom/winner-section.js
@@ -0,0 +1,15 @@
+export default function declareWinner(name) {
+  const container = document.createElement('div');
+  const header = document.createElement('h2');
+  const winner = document.createElement('h3');
+  const resetBtn = document.createElement('button');
+
+  container.classList.add('winner-section')
+  header.textContent = 'THE WINNER IS:';
+  winner.textContent = name;
+  resetBtn.textContent = 'PLAY AGAIN';
+  resetBtn.id = 'reset-btn';
+
+  container.append(header, winner, resetBtn);
+  return container;
+}
