62f0093acaf0fe26f165f7c76f84de573bf48d65
Will H <will.wkhu@gmail.com>
2023-04-03 21:32:19 -0700

    Remove everything except styles and init fn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './styles/reset.css';
 import './styles/styles.css';
-import startForm from './modules/dom/start-form';
 import initNewGame from './modules/controller';
 
-const main = document.querySelector('main');
-main.append(startForm());
 initNewGame();
+
+
 
