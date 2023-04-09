d5c8ddd39494f5862d21104f778e8f0312369c42
Will H <will.wkhu@gmail.com>
2023-04-08 23:22:52 -0700

    Add comments

--- src/modules/data/aux-helper-fns.js
+++ src/modules/data/aux-helper-fns.js
@@ -1,3 +1,4 @@
+// Return lowest num in row/column
 function getFloor(num, dir) {
   if (dir === 'x') {
     return num % 10 === 0 ? num - 9 : Math.floor(num / 10) * 10 + 1;
@@ -7,16 +8,21 @@ function getFloor(num, dir) {
   return num - Math.floor(num / 10) * 10;
 }
 
+// Returns max num in row/column
 function getCeiling(num, dir) {
   if (dir === 'x') return Math.ceil(num / 10) * 10;
   if (num % 10 === 0) return 100;
   return num + (100 - Math.ceil(num / 10) * 10);
 }
 
+// Randomly return x or y
+// For choosing axis
 function pickADir() {
   return Math.round(Math.random()) === 0 ? 'x' : 'y';
 }
 
+// Check if array of coords is allowed
+// Use to check if a ship's position is allowed
 function isCoordsEligible(parentArr, childArr, maxVal) {
   if (childArr.some((element) => parentArr.includes(element))) return false;
   if (childArr[childArr.length - 1] > maxVal) return false;
@@ -27,6 +33,8 @@ function genNmbr(limit) {
   return Math.floor(Math.random() * limit) + 1;
 }
 
+// Returns a ship's occupied coords(squares)
+// based on start coord and length
 function getPositionCoords(shipObj, start, axis) {
   const positionCoords = [];
 
