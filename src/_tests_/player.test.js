fee7da87431411ddedeef6693fc3bcdb08c0f3ab
Will H <will.wkhu@gmail.com>
2023-04-10 23:00:36 -0700

    Finish tests

--- src/_tests_/player.test.js
+++ src/_tests_/player.test.js
@@ -1,4 +1,5 @@
 import Player from '../modules/data/player';
+import { getCeiling } from '../modules/data/aux-helper-fns';
 
 /* eslint-disable */
 
@@ -24,8 +25,8 @@ describe('Player object', () => {
     });
   });
 
-  describe('Get random num', () => {
-    test('Return random num', () => {
+  describe('Get random Coord', () => {
+    test('Return random Coord', () => {
       expect(player1.getRandomCoord()).toBeLessThanOrEqual(100);
     });
 
@@ -38,24 +39,12 @@ describe('Player object', () => {
     });
   });
 
-  describe('What happens when attacking', () => {
-    test('Opponent receives attack', () => {
-      const player2 = new Player('player2');
-      player1.opponent = player2;
-
-      const receiveAttack = jest.spyOn(player2.board, 'receiveAttack');
-      player1.attack(2);
-      expect(receiveAttack).toHaveBeenCalled();
-    });
-
-    test('Attack is logged', () => {
-      expect(player1.attacks).toEqual([2]);
-    });
-  });
-
   describe('Player ships', () => {
-    test('Player has all ships', () => {
+    beforeAll(() => {
       player1.assembleFleet();
+    });
+
+    test('Player has all ships', () => {
       expect(player1.board.fleet).toEqual([
         { damage: [], length: 5, name: 'CARRIER', position: [] },
         { damage: [], length: 4, name: 'BATTLESHIP', position: [] },
@@ -64,55 +53,161 @@ describe('Player object', () => {
         { damage: [], length: 2, name: 'PATROL BOAT', position: [] },
       ]);
     });
+
+    describe('Fleet positioning', () => {
+      let fleet;
+      let fleetCoords;
+
+      beforeAll(() => {
+        player1.autoPositionFleet();
+        fleet = player1.board.fleet;
+        fleetCoords = fleet.reduce((coordArr, ship) => {
+          coordArr.push(...ship.position);
+          return coordArr;
+        }, []);
+      });
+
+      test('Correct number of coords', () => {
+        expect(fleetCoords).toHaveLength(17);
+      });
+
+      test('Ship positions are unique', () => {
+        const hasDupCoords = (arr) => arr.length !== new Set(arr).size;
+
+        expect(hasDupCoords(fleetCoords)).toBe(false);
+      });
+
+      test('Ship positions within max value (not off board)', () => {
+        // Returns last ship coord <= max val
+        const withinMax = (arr) => {
+          const dir = arr[0] + 1 === arr[1] ? 'x' : 'y';
+          const lastIndex = arr.length - 1;
+          const lastNum = arr[lastIndex];
+          const maxNum = getCeiling(arr[0], dir);
+          return lastNum <= maxNum;
+        };
+        // Test array with 1 ship position over max val
+        const badFleetPos = [
+          {
+            position: [1, 2, 3],
+          },
+          {
+            position: [4, 5, 11],
+          },
+        ];
+
+        expect(
+          badFleetPos.every((ship) => withinMax(ship.position) === true)
+        ).toBe(false);
+
+        expect(fleet.every((ship) => withinMax(ship.position) === true)).toBe(
+          true
+        );
+      });
+
+      test('Ship coords are sequential', () => {
+        // Checks if vals are consecutive by 1 or 10
+        const isSequential = (arr) => {
+          const toAdd = arr[0] + 1 === arr[1] ? 1 : 10;
+          for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] + toAdd != arr[i + 1]) return false;
+          }
+          return true;
+        };
+
+        // Test array with 1 ship position not consecutive
+        const badFleetPos = [
+          {
+            position: [1, 2, 3],
+          },
+          {
+            position: [10, 20, 30],
+          },
+          {
+            position: [10, 11, 20],
+          },
+        ];
+
+        expect(
+          badFleetPos.every((ship) => isSequential(ship.position) === true)
+        ).toBe(false);
+
+        expect(
+          fleet.every((ship) => isSequential(ship.position) === true)
+        ).toBe(true);
+      });
+
+      test('Clear ship positions', () => {
+        expect(fleet.every((ship) => ship.position.length < 1)).toBe(false);
+
+        player1.resetFleet();
+
+        expect(fleet.every((ship) => ship.position.length < 1)).toBe(true);
+      });
+    });
   });
 
-  // Combine with above
-  describe.only('Preparing fleet', () => {
-    let fleet;
-    let fleetCoords;
+  describe('What happens when attacking', () => {
+    let player2;
 
     beforeAll(() => {
-      player1.assembleFleet();
-      player1.autoPositionFleet();
-
-      fleet = player1.board.fleet;
-
-      console.log(player1);
-      fleetCoords = fleet.reduce((coordArr, ship) => {
-        coordArr.push(...ship.position);
-        return coordArr;
-      }, []);
+      player2 = new Player('player2');
+      player2.board.fleet = [
+        {
+          name: 'Submarine',
+          position: [4, 5, 6],
+        },
+        {
+          name: 'Destroyer',
+          position: [11, 12, 13],
+        },
+      ];
+      player2.board.shipsLost = [
+        {
+          name: 'Submarine',
+          position: [4, 5, 6],
+        },
+      ];
+      player1.opponent = player2;
     });
 
-    test('Correct number of coords', () => {
-      // player1.autoPositionFleet();
-      // const fleet = player1.board.fleet;
-      // const fleetCoords = fleet.reduce((coordArr, ship) => {
-      //   coordArr.push(...ship.position);
-      //   return coordArr;
-      // }, []);
-
-      expect(fleetCoords).toHaveLength(17);
+    test('Opponent receives attack', () => {
+      const receiveAttack = jest.spyOn(player2.board, 'receiveAttack');
+      player1.attack(2);
+      expect(receiveAttack).toHaveBeenCalled();
     });
 
-    test('Ship positions are unique', () => {
-      console.log(fleetCoords);
+    test('Attack is logged', () => {
+      expect(player1.attacks).toEqual([2]);
+    });
+
+    test('Report a miss', () => {
+      expect(player1.reportAttackResult(2)).toEqual('miss');
+    });
+
+    test('Report a hit', () => {
+      expect(player1.reportAttackResult(11)).toEqual('hit');
+    });
+
+    test('Report sunk', () => {
+      expect(player1.reportAttackResult(4)).toEqual('sunk');
+    });
+
+    test('Report destroyed', () => {
+      player2.board.fleet = [
+        {
+          name: 'Submarine',
+          position: [4, 5, 6],
+        },
+      ];
+      expect(player1.reportAttackResult(4)).toEqual('destroyed');
+    });
+
+    test('Identify hit ship', () => {
+      expect(player1.identifyEnemyShip(4)).toEqual({
+        name: 'Submarine',
+        position: [4, 5, 6],
+      });
     });
   });
-
-  // describe('Assembling fleet', () => {
-  //   test('Correct number of ships', () => {
-  //     player1.assembleFleet();
-  //     expect(player1.board.fleet).toHaveLength(5);
-  //   });
-
-  //   test('Fleet has a carrier', () => {
-  //     expect(player1.board.fleet).toContainEqual({
-  //       name: 'carrier',
-  //       length: 5,
-  //       position: [],
-  //       damage: [],
-  //     });
-  //   });
-  // });
 });
