e791a7517ea23ce634cced0df1e245619202b6bc
Will H <will.wkhu@gmail.com>
2023-04-11 14:16:50 -0700

    Add and display favicon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './styles/reset.css';
 import './styles/styles.css';
+import icon from './assets/images/favicon.png';
 import initNewGame from './modules/controller';
+import setFavicon from './modules/dom/display-favicon';
 
+setFavicon(icon);
 initNewGame();
